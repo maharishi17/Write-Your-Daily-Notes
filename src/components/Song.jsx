f25eb52c6d24a81fbded51f99a69b84c9f7c2ca8
maharishi17 <maharishi1711@gmail.com>
2024-10-18 09:57:37 +0530

    Updated Song component and added music file

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 function Audi() {
   useEffect(() => {
-    const music = new Audio('/Deema.mp3'); 
+    const music = new Audio('/music/Deema.mp3');
 
     const playButton = document.getElementById('play-music');
     const pauseButton = document.getElementById('pause-music');
