ec2da8f43c330c42defdb22ea9558a5d2b67fcac
maharishi17 <maharishi1711@gmail.com>
2024-10-18 09:29:47 +0530

    Updated base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Daily-Notes/',  // This must match your GitHub Pages URL
+  base: '/Write-Your-Daily-Notes/',  // This should match your GitHub Pages repo name
   plugins: [react()],
 })
